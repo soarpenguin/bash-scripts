11075639bb11df9566fc841b04526e6585e74476
soarpenguin <soarpenguin@gmail.com>
2022-01-29 21:45:27 +0800

    update: add kubectl usage cmd.

---
+++ kubectl.sh
@@ -0,0 +1,9 @@
+source <(kubectl completion zsh)  # 在 zsh 中设置当前 shell 的自动补全
+echo "[[ $commands[kubectl] ]] && source <(kubectl completion zsh)" >> ~/.zshrc
+
+kubectl proxy
+kubectl get --raw /
+kubectl get --raw /
+kubectl get --raw /api/v1
+
+kubectl api-resources
