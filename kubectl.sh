954226c57a700be775999c4a25e43becd2d831f8
soarpenguin <soarpenguin@gmail.com>
2022-02-18 15:20:36 +0800

    update: add kubectl usage cmd.

--- kubectl.sh
+++ kubectl.sh
@@ -6,4 +6,21 @@ kubectl get --raw /
 kubectl get --raw /
 kubectl get --raw /api/v1
 
+kubectl explain
 kubectl api-resources
+kubectl get pods
+kubectl describe pods my-pod
+
+# watch pod update
+kubectl get pod -w
+
+kubectl logs my-pod
+kubectl logs my-pod -c my-container
+# -f means follow logs output
+kubectl logs -f my-pod
+kubectl logs -f my-pod -c my-container
+
+# debug pod
+kubectl exec my-pod -- /bin/bash
+kubectl top pod POD_NAME --containers
+
